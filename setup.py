776c0ffac1b2c4721c469bc44f2a05aa31e21408
thasneem <thasneemjamal122@gmail.com>
2025-01-11 18:40:45 +0300

    setup file modified

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
-from setuptools import setup, find_packages
-
+# creating local package
+from setuptools import setup,find_packages
 setup(
-    name="us_visa",
-    version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
-    packages=find_packages()
+    name='us_visa',version="0.0",packages=find_packages(),author='IBRAHIM',
+    author_email='thasneemjamal122@gmail.com'
 )
