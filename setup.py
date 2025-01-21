9a148cc936166cf19f4c7c14e5c802cf417b4723
thasneem <thasneemjamal122@gmail.com>
2025-01-21 17:05:14 +0300

    setup file updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="THASNEEDB",
+    author_email="thasneemjamal122@gmail.com",
     packages=find_packages()
 )
